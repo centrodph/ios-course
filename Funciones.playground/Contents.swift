850fad5539fbb688ed8d7375cbbbfda9dd579e18
iOS <ios@MBP-DH-1.local>
2018-03-19 10:19:16 -0300

    funciones

---
+++ Funciones.playground/Contents.swift
@@ -0,0 +1,46 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+// _ quiere decir que no requiere etiqueta
+func saludar(a nombre: String, de empresa: String){
+    print("Welcome! \(nombre) de \(empresa)")
+}
+
+func saludar(_ nombre: String, _ empresa: String){
+    print("TEST \(nombre) de \(empresa)")
+}
+
+
+saludar(a:"Gerardo",de:"Accenture")
+saludar("Gerardo","Accenture")
+
+
+func encontrarMax(entre a:Int, y b: Int) -> Int{
+    return  ( a > b ? a : b)
+}
+
+var result = encontrarMax(entre: 78, y: -78)
+print(result)
+
+
+
+
+// funcion de sumar numeros
+
+func sumarPares(entre a: Int, y b: Int) -> Int{
+    var suma = 0;
+    for j in a...b{
+        if j % 2 == 0 {
+            suma = suma + j
+        }
+    }
+    return suma
+}
+
+print("suma de pares entre 1 y 100 \(sumarPares(entre: 1, y: 100))")
+
+
+
+
--- ImprimirNumerosNaturales.playground/Contents.swift
+++ ImprimirNumerosNaturales.playground/Contents.swift
@@ -2,6 +2,18 @@
 
 import UIKit
 
+var suma = 0;
 for i in 1...100{
-    print("numero natural \(i)")
+    suma = suma + i ;
 }
+
+print("Suma de los primeros 100 numeros naturales \(suma)")
+
+var sumaPares = 0;
+for j in 1...100{
+    if j % 2 == 0{
+        sumaPares = sumaPares + j;
+    }
+}
+
+print("Suma de los primeros 100 numeros naturales pares \(sumaPares)")
