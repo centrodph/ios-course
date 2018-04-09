0c4b0fdff880a89f1ad94d16f2645ce4ef2f6b00
Digital House <digitalhouse@MBP-DH-1.local>
2018-04-09 09:03:04 -0300

    Colores sliders

---
+++ ColoresConSliders/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  ColoresConSliders
+//
+//  Created by Andres Ciaño on 3/28/18.
+//  Copyright © 2018 Digital House. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var rSlider: UISlider!
+    @IBOutlet weak var gSlider: UISlider!
+    @IBOutlet weak var bSlider: UISlider!
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func update(_ sender: Any) {
+        view.backgroundColor = UIColor(
+            red: CGFloat(rSlider.value),
+            green: CGFloat(gSlider.value),
+            blue: CGFloat(bSlider.value),
+            alpha: 1.0)
+    }
+
+
+    @IBAction func rValueChanged(_ sender: Any) {
+        print("CAMBIO ROJO")
+//        view.backgroundColor = UIColor(
+//            red: CGFloat(rSlider.value),
+//            green: CGFloat(gSlider.value),
+//            blue: CGFloat(bSlider.value),
+//            alpha: 1.0)
+    }
+
+    @IBAction func gValueChanged(_ sender: Any) {
+        print("CAMBIO VERDE")
+//        view.backgroundColor = UIColor(
+//            red: CGFloat(rSlider.value),
+//            green: CGFloat(gSlider.value),
+//            blue: CGFloat(bSlider.value),
+//            alpha: 1.0)
+    }
+
+    @IBAction func bValueChanged(_ sender: Any) {
+        print("CAMBIO AZUL")
+//        view.backgroundColor = UIColor(
+//            red: CGFloat(rSlider.value),
+//            green: CGFloat(gSlider.value),
+//            blue: CGFloat(bSlider.value),
+//            alpha: 1.0)
+    }
+
+
+
+}
+
