d2d67d098d877d7fff8303b5acd9466a61a9e5b7
iOS <ios@MBP-DH-1.local>
2018-04-16 09:48:10 -0300

    Tableview part1

---
+++ TableViewProject/TableViewProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewProject
+//
+//  Created by iOS on 16/4/18.
+//  Copyright © 2018 Accenture. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
